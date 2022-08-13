f161206c3d0bd9cc37cfad55e823a46623c923f4
arhamrahim97 <moh.arham.rahim@gmail.com>
2022-08-13 12:02:36 +0800

    template perencanaan keong

---
+++ database/migrations/2022_08_13_114048_create_o_p_d_terkait_keong_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opd_terkait_keong', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('perencanaan_id'); // keong / hewan / manusia
+            $table->uuid('opd_id');
+            $table->string('nama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opd_terkait_keong');
+    }
+};
